313065bb857c918f5eda01ca36597def0c56b2e3
aljoud02 <aljoud027@@gmail.com>
2026-07-07 23:50:30 +0000

    Add task 9 print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 48;
+	while (num <= 57)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
