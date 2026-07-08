4d9f093191f48f49436b22753d957173e5590bc8
aljoud02 <aljoud027@@gmail.com>
2026-07-08 00:36:34 +0000

    Fix duplicate increment in multiplication_table.c

---
+++ intro_debugging/multiplication_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * print_table - Prints a multiplication table up to a given size
+ * @size: The size of the table matrix
+ *
+ * Return: void
+ */
+void print_table(int size)
+{
+	int row, col;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+		{
+			printf("%d ", row * col);
+		}
+		printf("\n");
+	}
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+	print_table(4);
+	return (0);
+}
