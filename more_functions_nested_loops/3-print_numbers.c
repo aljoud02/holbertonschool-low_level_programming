59d8a59395e43c33fb21a7c07833695f0d3db117
aljoud02 <aljoud027@@gmail.com>
2026-07-08 00:45:15 +0000

    Add 3-print_numbers.c and update main.h prototype

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Description: Uses a loop to print numbers from 0 to 9
+ * followed by a new line.
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
