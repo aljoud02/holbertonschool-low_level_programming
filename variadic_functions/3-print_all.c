7503f764731c6b91033416c9f38cda7384febcc4
aljoud02 <aljoud027@@gmail.com>
2026-07-29 17:17:48 +0000

    Fix Betty style and restrict if statements to 2

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,46 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - Prints a char.
+ * @arg: Arguments list.
+ */
+void print_char(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+
+/**
+ * print_int - Prints an int.
+ * @arg: Arguments list.
+ */
+void print_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * print_float - Prints a float.
+ * @arg: Arguments list.
+ */
+void print_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+ * print_string - Prints a string.
+ * @arg: Arguments list.
+ */
+void print_string(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
  * print_all - Prints anything based on a format string.
  * @format: A list of types of arguments passed to the function.
  * @...: A variable number of arguments to be printed.
@@ -8,32 +48,30 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
-	char *str, *sep = "";
+	int i = 0, j = 0;
+	char *sep = "";
+	token_t funcs[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
 
 	va_start(args, format);
 
 	while (format != NULL && format[i] != '\0')
 	{
-		while (format[i] == 'c' || format[i] == 'i' ||
-		       format[i] == 'f' || format[i] == 's')
+		j = 0;
+		while (funcs[j].symbol != '\0')
 		{
-			printf("%s", sep);
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			if (format[i] == 's')
+			if (format[i] == funcs[j].symbol)
 			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s", str);
+				printf("%s", sep);
+				funcs[j].print(args);
+				sep = ", ";
 			}
-			sep = ", ";
-			break;
+			j++;
 		}
 		i++;
 	}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,6 +5,17 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * struct token - Struct token
+ * @symbol: The data type token (c, i, f, s)
+ * @print: The function pointer to print the data type
+ */
+typedef struct token
+{
+	char symbol;
+	void (*print)(va_list arg);
+} token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
